124917ea4297b06d7fab0cda31969d1ac0aa6b01
Digitql0 <imonkix@gmail.com>
2025-08-05 12:23:51 +0200

    Adjusted Variable and Function Names

--- include/MostlyForces.hpp
+++ include/MostlyForces.hpp
@@ -38,23 +38,23 @@ typedef struct {
 float clampValue (float value, float min, float max);
 
 //Vector Math
-float getMagnitude(MF_vec2 vec);
-float getDistance(MF_vec2 p1, MF_vec2 p2);
-MF_vec2 getZerovec2();
-MF_vec3 getZeroVec3();
-MF_vec2 getSubtractedVec(MF_vec2 a, MF_vec2 b);
-MF_vec2 getAddedVec(MF_vec2 a, MF_vec2 b);
-MF_vec2 getScaledVec(MF_vec2 vec, float scalar);
-MF_vec2 getNormalizedVec(MF_vec2 vec);
+float MF_getMagnitude(MF_vec2 vec);
+float MF_getDistance(MF_vec2 p1, MF_vec2 p2);
+MF_vec2 MF_getZerovec2();
+MF_vec3 MF_getZeroVec3();
+MF_vec2 MF_getSubtractedVec(MF_vec2 a, MF_vec2 b);
+MF_vec2 MF_getAddedVec(MF_vec2 a, MF_vec2 b);
+MF_vec2 MF_getScaledVec(MF_vec2 vec, float scalar);
+MF_vec2 MF_getNormalizedVec(MF_vec2 vec);
 
 //Collision Detection
-bool CheckCollisionPointInBox(MF_vec2 point, MF_rectangle box);
-bool CheckCollisionCircles(MF_circle c1, MF_circle c2);
-bool CheckCollisionRectangles(MF_rectangle r1, MF_rectangle r2);
-bool CheckCollisionPointInCircle(MF_vec2 point, MF_circle c);
-bool CheckCollisionCircleInRectangle(MF_circle c, MF_rectangle r);
+bool MF_checkCollisionPointInBox(MF_vec2 point, MF_rectangle box);
+bool MF_checkCollisionCircles(MF_circle c1, MF_circle c2);
+bool MF_checkCollisionRectangles(MF_rectangle r1, MF_rectangle r2);
+bool MF_checkCollisionPointInCircle(MF_vec2 point, MF_circle c);
+bool MF_checkCollisionCircleInRectangle(MF_circle c, MF_rectangle r);
 
 //Collision Response
-void ResponseCollisionCircles(MF_circle c1, MF_circle c2, MF_vec2& v1, MF_vec2& v2);
+void MF_responseCollisionCircles(MF_circle c1, MF_circle c2, MF_vec2& v1, MF_vec2& v2);
 
 #endif
